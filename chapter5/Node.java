5622a9fcfcc508bcb5176108e8fdb9a185294cd1
yangchao830925 <hbdxych@163.com>
2020-06-29 16:22:42 +0800

    Create Node.java

---
+++ chapter5/Node.java
@@ -0,0 +1,32 @@
+package org.infoq.com.ch5;
+
+/**
+ * Created by yangchao on 2020/6/28.
+ */
+public class Node {
+
+    private String ip;
+    private String name;
+
+    public Node(String ip, String name) {
+        this.ip = ip;
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
